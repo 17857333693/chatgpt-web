f869db3f13d588052dc035cb10e343c2d2686f1f
17857333693 <105497517+17857333693@users.noreply.github.com>
2024-05-08 16:28:07 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
     url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: "sk-proj-ABL0ncYuBt523L7DcjpsT3BlbkFJs7pkfw9uzXE6EVGAjJPJ"  // openAi Base64 编码 apiKey
+    apiKey: "c2stcHJvai1BQkwwbmNZdUJ0NTIzTDdEY2pwc1QzQmxia0ZKczdwa2Z3OXV6WEU2RVZHQWpKUEo="  // openAi Base64 编码 apiKey
 };  
